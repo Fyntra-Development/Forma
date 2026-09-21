b7ab49348882118b17cb9f44f980e8be3f89f670
Fyntra-Development <129816818+Fyntra-Development@users.noreply.github.com>
2026-09-20 22:57:47 -0600

    Updated Library.lua

--- Library.lua
+++ Library.lua
@@ -207,7 +207,7 @@ local Library = {
 
     -- Built-in update system. Component versions are compared against versions.json
     -- on the Forma repository whenever the library or a manager is opened.
-    Version = '1.3.4';
+    Version = '1.3.5';
     AutoUpdateVersion = 1;
     AutoUpdateEnabled = true;
     UpdateRepoBaseUrl = RepoBaseUrl;
